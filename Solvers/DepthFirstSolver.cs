8a82e2c3933ed5c74384631364dfb4a52eba5eca
jbaldwin <joshrbaldwin@gmail.com>
2013-04-11 16:11:04 -0600

    LGPL v3.0 Licensed

--- Solvers/DepthFirstSolver.cs
+++ Solvers/DepthFirstSolver.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/** 
+ * Boggle Solver
+ * Copyright (C) - 2013 Josh Baldwin - https://github.com/jbaldwin/boggle
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +28,9 @@ namespace boggle.Solvers
 {
 	public class DepthFirstSolver : ISolver
 	{
+		/// <summary>
+		/// All 8 neighbor combinations.
+		/// </summary>
 		private static int[][] _neighbors = new int[][] {
 			new int[] { -1, -1 },
 			new int[] {  0, -1 },
@@ -27,6 +49,11 @@ namespace boggle.Solvers
 
 		}
 
+		/// <summary>
+		/// Solves the boggle board!
+		/// </summary>
+		/// <param name="board">Board to solve.</param>
+		/// <param name="library">Dictionary of words.</param>
 		public Result Solve(IBoard board, ILibrary library)
 		{
 			var result = new Result();
@@ -42,11 +69,19 @@ namespace boggle.Solvers
 					// Capture i/j values (will fail without since i/j change too fast before the thread runs)
 					var state = new ThreadState(i, j);
 
+					// note: I'm sure this is a lot of memory spawning a bajillion tasks but it was easy.
 					tasks.Add(Task.Run(() =>
 					{
 
 						var path = new WordPath(board.Grid[state.X][state.Y], state.X, state.Y);
-						var myWords = Worker(state.X, state.Y, path, 1, board, library.Books[board.Grid[state.X][state.Y]], new HashSet<string>());
+						var myWords = Worker(
+							state.X, 
+							state.Y, 
+							path, 
+							1, 
+							board, 
+							library.Books[board.Grid[state.X][state.Y]], 
+							new HashSet<string>());
 
 						// Merge results (queue up instead and have the main thread do the work?
 						// This seems like a bottle neck but since tasks are not reused it might not matter.
@@ -67,7 +102,26 @@ namespace boggle.Solvers
 			return result;
 		}
 
-		private HashSet<string> Worker(int x, int y, WordPath path, int depth, IBoard board, ILetter parent, HashSet<string> words)
+		/// <summary>
+		/// Worker thread for a single square in the boggle grid.
+		/// </summary>
+		/// <param name="x">X grid position.</param>
+		/// <param name="y">Y grid position.</param>
+		/// <param name="path">Word Path object.</param>
+		/// <param name="depth">Current number of squares deep, maximum is 32.</param>
+		/// <param name="board">The boggle board.</param>
+		/// <param name="parent">The parent node, when first called this is the first letter in the word's book from the dictionary.</param>
+		/// <param name="words">The words this worker has found on the boggle board.</param>
+		/// <returns></returns>
+		private HashSet<string> Worker(
+			int x, 
+			int y, 
+			WordPath 
+			path, int 
+			depth, 
+			IBoard board, 
+			ILetter parent, 
+			HashSet<string> words)
 		{
 			var c = board.Grid[x][y];
 
@@ -108,7 +162,7 @@ namespace boggle.Solvers
 						}
 					}
 
-					// maximum recusion should be maximum word length (28~)
+					// maximum recursion should be maximum word length (28~)
 					Worker(nX, nY, path, depth + 1, board, child, words);
 				}
 			}
@@ -117,6 +171,9 @@ namespace boggle.Solvers
 		}
 	}
 
+	/// <summary>
+	/// This class is to capture the X and Y grid coordinates for a worker thread.
+	/// </summary>
 	class ThreadState
 	{
 		public volatile int X;
@@ -141,17 +198,27 @@ namespace boggle.Solvers
 		}
 	}
 
+	/// <summary>
+	/// Each worker thread is given a word path.  A word path is the letters from each square on the grid to make a word.
+	/// Since boggle does not let you re-use letters this word path remembers all the parent squares and their letters
+	/// in two 32 sized arrays, each element cooresponding to the depth of the word path.
+	/// The word path only goes to a depth of 32 since the maximum word size in the enable1 dictionary is 28 (or 27 can't remember).
+	/// If you have longer words... up the depth count.
+	/// Note: this does not check for overflow on the MAX_DEPTH when performing actions so verify your dictionary's longest word or you could get a runtime error.
+	/// </summary>
 	class WordPath
 	{
+		public static readonly int MAX_DEPTH = 32;
+
 		public byte[] Letters;
 		public Point[] Parents;
 
 		public WordPath(byte c, int x, int y)
 		{
-			Letters = new byte[32];
+			Letters = new byte[MAX_DEPTH];
 			Letters[0] = c;
 
-			Parents = new Point[32];
+			Parents = new Point[MAX_DEPTH];
 			Parents[0].X = x;
 			Parents[0].Y = y;
 		}
